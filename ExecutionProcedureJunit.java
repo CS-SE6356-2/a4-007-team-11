27f55b0b7bcbe09c94148c2b65feaf414414e2b1
Jackson Small <Jackson Small@10.21.12.202>
2019-04-08 15:42:53 -0500

    Creation of ExecutionProcedureJunit, and TestJunit

---
+++ ExecutionProcedureJunit.java
@@ -0,0 +1,36 @@
+import org.junit.After;
+import org.junit.AfterClass;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+public class ExecutionProcedureJunit {
+	@BeforeClass
+	public static void beforeClass() {
+		
+	}
+	
+	@AfterClass
+	public static void afterClass() {
+		
+	}
+	
+	@Before
+	public void before() {
+		
+	}
+	
+	@After
+	public void after() {
+		
+	}
+	
+	@Test
+	public void test() {
+		
+	}
+	
+}
