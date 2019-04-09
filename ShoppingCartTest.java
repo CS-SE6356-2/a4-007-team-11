89bc70e85783b006d76658e282474fe51c7d9a73
JSmalls96 <37056289+JSmalls96@users.noreply.github.com>
2019-04-08 20:49:09 -0500

    Creation of the ShoppingCartTest structure, and edit to .project file

---
+++ ShoppingCartTest.java
@@ -0,0 +1,38 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ShoppingCartTest {
+
+	//test for "When created, the cart has 0 items"
+	@Test
+	void createZeroItemsCartTest() {
+		fail("Not yet implemented");
+	}
+	
+	//test for "when empty, the cart has 0 items"
+	@Test
+	void cartEmptyZeroItemsTest() {
+		
+	}
+	
+	//test for the removeItem() function, ensuring number of items decremented
+	//"When an item is removed, the number of items must be decreased"
+	@Test
+	void itemRemovedTest() {
+		
+	}
+	
+	//test for "When a new product is added, the number of items must be incremented"
+	@Test
+	void newProductAddedItemsIncrementTest() {
+		fail("Not yet implemented");
+	}
+	
+	//test for "When a new product is added, the new balance must be the sum of the previous balance plus the cost of the new product"
+	@Test
+	void newProductAddedBalanceUpdateTest() {
+		
+	}
+
+}
--- Test_runner.java
+++ Test_runner.java
@@ -4,7 +4,7 @@ import org.junit.runner.notification.Failure;
 
 public class Test_runner {
 	public static void main(String[] args) {
-		Result result = JUnitCore.runClasses(TestJunit.class);
+		Result result = JUnitCore.runClasses(ShoppingCartTest.class);
 		for(Failure failure : result.getFailures()) {
 			System.out.println(failure.toString());
 		}
