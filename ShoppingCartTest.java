37d45cea7a7fed0ca9abfbd599f8700739f27e75
atravisfields <47760837+atravisfields@users.noreply.github.com>
2019-04-08 21:42:35 -0500

    Finished ShoppingCart test cases

--- ShoppingCartTest.java
+++ ShoppingCartTest.java
@@ -6,22 +6,34 @@ public class ShoppingCartTest {
 
 	//test for "When created, the cart has 0 items"
 	@Test
-	public void createZeroItemsCartTest() {
+	void createZeroItemsCartTest() {
 		ShoppingCart cart=new ShoppingCart();
-		assertEquals(0,cart.getItemCount());
+		assertEquals("When created, the cart should have 0 items", 0, cart.getItemCount());
 	}
 	
 	//test for "when empty, the cart has 0 items"
 	@Test
-	public void cartEmptyZeroItemsTest() {
+	void cartEmptyZeroItemsTest() {
+		ShoppingCart cart = new ShoppingCart();
+		Product exProduct = new Product("example", 1);
+		cart.addItem(exProduct);
+		cart.removeItem(exProduct);
 		
+		assertEquals( "When empty, the cart should have 0 items", 0, cart.getItemCount() );
 	}
 	
 	//test for the removeItem() function, ensuring number of items decremented
 	//"When an item is removed, the number of items must be decreased"
 	@Test
-	public void itemRemovedTest() {
+	void itemRemovedTest() {
+		ShoppingCart cart1 = new ShoppingCart();
+		ShoppingCart cart2 = new ShoppingCart();
+		Product exProduct = new Product("example", 1);
+		cart1.addItem(exProduct);
+		cart2.addItem(exProduct);
+		cart2.removeItem(exProduct);
 		
+		assertTrue( "When an item is removed, the number of items should be decreased", cart2.getItemCount() < cart1.getItemCount() );
 	}
-	
+
 }
