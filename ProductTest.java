81441365f14d1df68b206d7dc814bf796ce96e6e
dingoose <vihasg32@gmail.com>
2019-04-08 22:23:45 -0500

    Created a J unit to handle the cases for when a product is added to the shopping cart and when the desired changes of incrementing the number of items and adding the cost of the item to the total shopping cart balance

--- ProductTest.java
+++ ProductTest.java
@@ -6,14 +6,28 @@ public class ProductTest {
 
 	//test for "When a new product is added, the number of items must be incremented"
 	@org.junit.Test
-	public void newProductAddedItemsIncrementTest() {
-		fail("Not yet implemented");
+	public void newProductAddedItemsIncrementTest() 
+	{
+		ShoppingCart example = new ShoppingCart();
+		int numItems = example.getItemCount();
+		example.addItem(new Product("Some Product", 100.00));
+		assertEquals(numItems + 1, example.getItemCount());
+		numItems++;
+		example.addItem(new Product("Another Product to check when list is greater than 0", 200.00));
+		assertEquals(numItems + 1, example.getItemCount());
 	}
 	
 	//test for "When a new product is added, the new balance must be the sum of the previous balance plus the cost of the new product"
 	@org.junit.Test
-	public void newProductAddedBalanceUpdateTest() {
-		
+	public void newProductAddedBalanceUpdateTest() 
+	{
+		ShoppingCart example = new ShoppingCart();
+		double balance = example.getBalance();
+		example.addItem(new Product("Some Product", 100.00));
+		assertEquals(balance + 100.00, example.getBalance());
+		balance = example.getBalance();
+		example.addItem(new Product("Another Product to check when list is greater than 0", 200.00));
+		assertEquals(balance + 200.00, example.getBalance());		
 	}
 
 }
